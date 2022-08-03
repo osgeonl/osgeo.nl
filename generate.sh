fb075d23cde07f4471fc720ae84fe73b497999bc
Just van den Broecke <just@justobjects.nl>
2022-08-03 11:24:29 +0200

    #2 first version auto publish

--- generate.sh
+++ generate.sh
@@ -5,11 +5,15 @@ set -e
 
 printf "\033[0;32mGenerating...\033[0m\n"
 
-/bin/rm -rf dist/*  > /dev/null 2>&1
-mkdir -p dist
+# /bin/rm .hugo_build.lock > /dev/null 2>&1
+
+TARGET_DIR="./public"
+/bin/rm -rf ${TARGET_DIR} > /dev/null 2>&1
+mkdir -p ${TARGET_DIR}
 
 # Build the project.
 # hugo - if using a theme, replace with `hugo -t <YOURTHEME>`
 hugo -D -t hugo-clarity
 
-echo "new.osgeo.nl" > dist/CNAME
+echo "new.osgeo.nl" > ${TARGET_DIR}/CNAME
+echo "ALL DONE"
